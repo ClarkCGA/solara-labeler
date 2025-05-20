32a173e33a0fff1d489805b6479e4229b4a5446b
Denys Godwin <denyswgodwin@gmail.com>
2025-05-20 00:27:44 +0000

    Added button to get next chip

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM jupyter/scipy-notebook:latest
 RUN mamba install -c conda-forge leafmap geopandas "localtileserver>=0.10.0" osmnx -y && \
-    pip install -U leafmap jsonschema==4.18.0 jupyter-server-proxy==4.4.0  lonboard h5py xarray solara pydantic && \
+    pip install -U leafmap jsonschema==4.18.0 lonboard h5py xarray solara pydantic && \
     fix-permissions "${CONDA_DIR}" && \
     fix-permissions "/home/${NB_USER}"
 
 ENV PROJ_LIB='/opt/conda/share/proj'
-ENV LOCALTILESERVER_CLIENT_PREFIX='proxy/{port}'
 
 RUN mkdir ./pages
 COPY /pages ./pages
--- pages/01_leafmap.py
+++ pages/01_leafmap.py
@@ -2,35 +2,93 @@ import leafmap
 import solara
 from urllib.parse import quote
 from localtileserver import TileClient
+import os
+from pathlib import Path
+import pandas as pd
+import geopandas as gpd
+from shapely import Polygon
+import rioxarray as rxr
+import ipywidgets as widgets
+import requests
+from urllib.parse import quote
 
-zoom = solara.reactive(2)
-center = solara.reactive((20, 0))
+data_dir = Path('/home/jovyan/data/')
 
+# Display Styles
+styledict = {
+            "stroke": True,
+            "color": "#FF0000",
+            "weight": 3,
+            "opacity": 1,
+            "fill": False,
+        }
+hover_style_dict = {
+    "weight": styledict["weight"],
+    "fillOpacity": 0,
+    "color": styledict["color"],
+}
 
 server = TileClient(
-    "/home/jovyan/data/vrt_output_mosaic.tif",
+    "/home/jovyan/data/2023/vrt_output_mosaic.tif",
     port=8888,
     host="0.0.0.0",
 )
 
-class Map(leafmap.Map):
+zoom = solara.reactive(19)
+center = solara.reactive((42.251504, -71.823585))
+current_chip = solara.reactive(None)
+
+def add_widgets(m, data_dir, styledict, hover_style_dict):
+    chip_button = widgets.Button(description="Next Chip")
+
+    def next_chip(b):
+        chips = pd.read_csv(data_dir / 'chips_with_ortho_id.csv')
+
+        # Get a random chip with status 'label'
+        labeled_chips = chips[chips['status'] == 'label']
+
+        chip = labeled_chips.sample(1)
+        chips.loc[chip.index, 'status'] = 'active'
+        chips.to_csv(data_dir / 'chips_with_ortho_id.csv', index=False)
+
+        chip['geometry'] = chip['geometry'].apply(lambda coord_str: Polygon(eval(coord_str)))
+        chip['TILENAME'] = chip['TILENAME'].apply(eval)
+
+        chip_gdf = gpd.GeoDataFrame(chip, geometry='geometry', crs='EPSG:6348').to_crs('EPSG:3857')
+        c = [c[0] for c in chip_gdf.to_crs('EPSG:4326').iloc[0].geometry.centroid.coords.xy]
+        for layer in list(m.layers):
+            if layer.name == 'chip':  # Only remove layers with the specific name
+                m.remove_layer(layer)
+        # Add new chip to map
+        m.add_gdf(
+            chip_gdf,
+            zoom_to_layer=False,
+            style=styledict,
+            hover_style=hover_style_dict,
+            layer_name='chip',
+            info_mode=None
+        )
+        center.set((c[1], c[0]))
+        zoom.set(19)
+
+        setattr(m, "gdf", chip_gdf)
+
+    chip_button.on_click(next_chip)
+    m.add_widget(chip_button)
+
+class LabelMap(leafmap.Map):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        file_url = quote("/home/jovyan/data/vrt_output_mosaic.tif", safe='')
+        file_url = quote("/home/jovyan/data/2023/vrt_output_mosaic.tif", safe='')
         tile_url = f'http://140.232.230.80:8851/api/tiles/{{z}}/{{x}}/{{y}}.png?&filename={file_url}'
 
         self.add_tile_layer(url=tile_url, name="Local Tiles", attribution="Local Tile Server")
-
-        self.add_stac_gui()
+        add_widgets(self, data_dir, styledict, hover_style_dict)
 
 @solara.component
 def Page():
     with solara.Column(style={"min-width": "500px"}):
-        # solara components support reactive variables
-        # solara.SliderInt(label="Zoom level", value=zoom, min=1, max=20)
-        # using 3rd party widget library require wiring up the events manually
-        # using zoom.value and zoom.set
-        Map.element(  # type: ignore
+        LabelMap.element(
             zoom=zoom.value,
             on_zoom=zoom.set,
             center=center.value,
@@ -40,5 +98,5 @@ def Page():
             data_ctrl=False,
             height="780px",
         )
-        # solara.Text(f"Zoom: {zoom.value}")
-        # solara.Text(f"Center: {center.value}")
+    
+    
